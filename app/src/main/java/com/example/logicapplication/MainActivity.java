4cf9cc8ab803e447c93f30ba5c2e2522d3575858
kyaw <kyawsithungalay@gmail.com>
2019-08-08 01:22:33 +0800

    Updated with main page

--- app/src/main/java/com/example/logicapplication/MainActivity.java
+++ app/src/main/java/com/example/logicapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.logicapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AsyncToServer.IServerResponse {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnSet;
     Spinner spinner;
     ArrayList<DepartmentStaff> list;
@@ -26,75 +27,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSet=(Button) findViewById(R.id.btnSet);
+        btnSet=(Button) findViewById(R.id.goBtn);
         btnSet.setOnClickListener(this);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        cmd = new Command(this, "get", "http://10.0.2.2:64451/DeptHead/GetStaffList", null);
-        new AsyncToServer().execute(cmd);
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        cmd = new Command(this, "get", "http://10.0.2.2:64451/DeptHead/GetStaffList", null);
+//        new AsyncToServer().execute(cmd);
+//    }
 
     @Override
     public void onClick(View v) {
 //        Command cmd;
 //
         int id = v.getId();
-        JSONArray array=new JSONArray();
-        JSONObject jsonObj = new JSONObject();
-        try {
-            DepartmentStaff staff=(DepartmentStaff) spinner.getSelectedItem();
-            String staffname=staff.name;
-           jsonObj.put("staffname", staffname);
-            array.put(jsonObj);
-        }
-        catch (Exception e) {
-           e.printStackTrace();
-        }
-        cmd = new Command(this, "set", "http://10.0.2.2:64451/DeptHead/AppointRep",array);
-        new AsyncToServer().execute(cmd);
+        Intent intent=new Intent(this,AppointRepActivity.class);
+        startActivity(intent);
+//        JSONArray array=new JSONArray();
+//        JSONObject jsonObj = new JSONObject();
+//        try {
+//            DepartmentStaff staff=(DepartmentStaff) spinner.getSelectedItem();
+//            String staffname=staff.name;
+//           jsonObj.put("staffname", staffname);
+//            array.put(jsonObj);
+//        }
+//        catch (Exception e) {
+//           e.printStackTrace();
+//        }
+//        cmd = new Command(this, "set", "http://10.0.2.2:64451/DeptHead/AppointRep",array);
+//        new AsyncToServer().execute(cmd);
     }
-
-    @Override
-    public void onServerResponse(Command jsonObj) {
-        if (jsonObj == null)
-            return;
-        spinner=this.findViewById(R.id.EmployeeName);
-        list=new ArrayList<DepartmentStaff>();
-        try {
-            String context = (String) jsonObj.context;
-            JSONArray j=jsonObj.data;
-            if (context.compareTo("get") == 0)
-            {
-                for(int i=0;i<j.length();i++){
-                    JSONObject obj=j.getJSONObject(i);
-                    String name=obj.getString("text");
-                    int id=obj.getInt("id");
-                    list.add(new DepartmentStaff(id,name));
-                }
-                spinner.setAdapter(new ArrayAdapter<DepartmentStaff>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, list));
-//                System.out.println("id: " + name + ", text: " + age);
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                        String employee=   spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString();
-                        Toast.makeText(getApplicationContext(),employee,Toast.LENGTH_LONG).show();
-                    }
-                    @Override
-                    public void onNothingSelected(AdapterView<?> adapterView) {
-                        // DO Nothing here
-                    }
-                });
-
-            }else if(context.compareTo("set")==0){
-                String status = (String)jsonObj.data.getJSONObject(0).get("text");
-                System.out.println("status:" + status);
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//
+//    @Override
+//    public void onServerResponse(Command jsonObj) {
+//        if (jsonObj == null)
+//            return;
+//        spinner=this.findViewById(R.id.EmployeeName);
+//        list=new ArrayList<DepartmentStaff>();
+//        try {
+//            String context = (String) jsonObj.context;
+//            JSONArray j=jsonObj.data;
+//            if (context.compareTo("get") == 0)
+//            {
+//                for(int i=0;i<j.length();i++){
+//                    JSONObject obj=j.getJSONObject(i);
+//                    String name=obj.getString("text");
+//                    int id=obj.getInt("id");
+//                    list.add(new DepartmentStaff(id,name));
+//                }
+//                spinner.setAdapter(new ArrayAdapter<DepartmentStaff>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, list));
+////                System.out.println("id: " + name + ", text: " + age);
+//                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//                    @Override
+//                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                        String employee=   spinner.getItemAtPosition(spinner.getSelectedItemPosition()).toString();
+//                        Toast.makeText(getApplicationContext(),employee,Toast.LENGTH_LONG).show();
+//                    }
+//                    @Override
+//                    public void onNothingSelected(AdapterView<?> adapterView) {
+//                        // DO Nothing here
+//                    }
+//                });
+//
+//            }else if(context.compareTo("set")==0){
+//                String status = (String)jsonObj.data.getJSONObject(0).get("text");
+//                System.out.println("status:" + status);
+//            }
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
